c14899c7ead288c411ad265ec050342fe914db52
Sok Kimsour <sok.kimsour12042003@gmail.com>
2026-05-26 13:13:10 +0700

    refactor: replace protect middleware with authenticateToken in certificate route

--- src/routes/Certificate.route.js
+++ src/routes/Certificate.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getMyCertificates, verifyCertificate } = require('../controller/Certificate.controller');
-const { protect } = require('../middlewares/authMiddleware'); 
+const { authenticateToken } = require('../middlewares/authMiddleware'); 
 
 
-router.get('/certificates/me', protect, /* #swagger.tags = ['Certificates'] */ getMyCertificates);
+router.get('/certificates/me', authenticateToken, /* #swagger.tags = ['Certificates'] */ getMyCertificates);
 
 router.get('/certificates/verify/:verificationId', /* #swagger.tags = ['Certificates'] */ verifyCertificate);
 
